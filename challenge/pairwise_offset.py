3ab0a26d6e546ed61af95bedb7a23db4937c9dd9
Sereas <65492966+Sereas@users.noreply.github.com>
2022-11-27 16:04:32 +0300

    completed pairwise offset task

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,20 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offset(sequence, fillvalue='*', offset=0):
+    total_length = len(sequence) + offset
+    return_list = []
+    for elem in range(total_length):
+        if elem > len(sequence)-1:
+            first_value = fillvalue
+        else:
+            first_value = sequence[elem]
+
+        if elem < offset:
+            second_value = fillvalue
+        else:
+            second_value = sequence[elem-offset]
+
+        new_tuple = (first_value, second_value)
+        return_list.append(new_tuple)
+        print(return_list)
+    return return_list
+
+pairwise_offset([(1, 2), (3, 4), (5, 6)], offset=4)
