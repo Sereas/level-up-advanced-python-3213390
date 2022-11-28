916ff422cded5828ee483e9d7ea47872a721b6b4
Sereas <65492966+Sereas@users.noreply.github.com>
2022-11-28 15:32:28 +0300

    completed medal task

--- challenge/medals.py
+++ challenge/medals.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import pandas as pd
 
 with open('olympics.txt', 'rt', encoding='utf-8') as file:    
     olympics = file.read()
@@ -6,8 +7,29 @@ with open('olympics.txt', 'rt', encoding='utf-8') as file:
 medal = namedtuple('medal', ['City', 'Edition', 'Sport', 'Discipline', 'Athlete', 'NOC', 'Gender',
        'Event', 'Event_gender', 'Medal'])
 
-medals = [] #Complete this - medals is a list of medal namedtuples
+medals = pd.read_csv('olympics.txt', sep=";")
+
+
+def get_query(key, value):
+    query = key + ' == "' + value + '" & '
+    return query
+
 
 def get_medals(**kwargs):
     '''Return a list of medal namedtuples '''
-    pass
+    total_query = ''
+    for key, value in kwargs.items():
+        query = get_query(key, value)
+        total_query += query
+
+    total_query = total_query[:-3]
+    df = medals.query(total_query)
+    df = df.astype({'Edition': 'str'})
+    return list(df.itertuples(name='medal', index=False))
+
+actual = len(get_medals(Edition='1984'))
+expected = 1459
+print(actual)
+print(expected)
+print(actual == expected)
+
